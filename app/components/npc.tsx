e824d8de3fd16fd02088bec802372ab5ca9805ad
zoan <104385984+zoan37@users.noreply.github.com>
2024-12-29 01:49:35 -0500

    add info on clicking objects

--- app/components/npc.tsx
+++ app/components/npc.tsx
@@ -1831,6 +1831,14 @@ const Scene = () => {
                                         <span>Click + Drag to look</span>
                                     </div>
                                     <div className="flex items-center gap-2">
+                                        <div className="flex items-center">
+                                            <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+                                                <path fillRule="evenodd" d="M6.672 1.911a1 1 0 10-1.932.518l.259.966a1 1 0 001.932-.518l-.26-.966zM2.429 4.74a1 1 0 10-.517 1.932l.966.259a1 1 0 00.517-1.932l-.966-.26zm8.814-.569a1 1 0 00-1.415-1.414l-.707.707a1 1 0 101.415 1.415l.707-.708zm-7.071 7.072l.707-.707A1 1 0 003.465 9.12l-.708.707a1 1 0 101.415 1.415zm3.2-5.171a1 1 0 00-1.3 1.3l4 10a1 1 0 001.823.075l1.38-2.759 3.018 3.02a1 1 0 001.414-1.415l-3.019-3.02 2.76-1.379a1 1 0 00-.076-1.822l-10-4z" clipRule="evenodd" />
+                                            </svg>
+                                        </div>
+                                        <span>Click certain objects to view details</span>
+                                    </div>
+                                    <div className="flex items-center gap-2">
                                         <kbd className="px-2 py-1 bg-gray-700 rounded">ESC</kbd>
                                         <span>Exit chat</span>
                                     </div>
